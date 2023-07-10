c09c11d5d5374569cf426cddfdfe870fa4d1e536
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-07-10 23:16:48 +0530

    added styling and component structure of header and home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Header from "./components/header/header";
+import Home from "./components/home/home";
+import Detail from "./components/details/detail";
+import PageNotFound from "./components/pageNotFound/pageNotFound";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/movie/:imdbID" element={<Detail />} />
+            <Route exact path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
